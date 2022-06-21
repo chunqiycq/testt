7583b03e63beeabaafb2666438dc226419cfa864
chunqi.yang <you@chunqi.yang@smartmore.com>
2022-06-21 10:34:04 +0800

    feat:客户端文件上传

--- client/clientget.go
+++ client/clientget.go
@@ -1,34 +1,45 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"net"
+	"net/http"
 	"os" //这里可以考虑使用缓冲写入的bufio？
 )
 
-func clientaddget(name, code string) {
+type userInfo struct {
+	Name string
+	Code string
+}
+
+func clientaddget(opera client) {
 	// 客户端连接服务器
-	dial, err := net.Dial("tcp", "127.0.0.1:8060") //网络连接函数
-	errFunction("net.Dial err：", err)
+	dial, err := net.Dial("tcp", "127.0.0.1:8060") //网络连接函数,tcp连接
+
+	errFunction("net.Dial err:", err)
 	defer dial.Close()
 	if err != nil {
-		fmt.Println("客户端未连接服务器")
+		fmt.Println("客户端未连接服务器", err)
 	}
 	fmt.Println("客户端成功连接服务器")
 	// 模拟浏览器
-	//fmt.Println("姓名密码：", "姓名密码：", name, code)
-	requstHttpHeader := "POST /chunqi?nameyangc HTTP/1.1\r\nHost:127.0.0.1:8060\r\nContent-Type:application/x-www-form-urlencoded\r\nContent-Length: 125\r\n\r\n吕大强" //声明并且赋值
-	fmt.Println(requstHttpHeader)
+	url2 := "http://127.0.0.1:8060/chunqi"
+	//var user userInfo
+	//user.Name = name
+	//user.Code = code
+	//body := "{" + "\"flagnum\":1," + "\"name\":" + "\"" + name + "\"" + ",\"code\":" + "\"" + code + "\"" + "}"
+	body := tojson(opera)
+	resp, err2 := http.Post(url2, "application/x-www-form-urlencoded", bytes.NewBuffer([]byte(body)))
+	fmt.Println(body)
+	fmt.Println(bytes.NewBuffer([]byte(body)))
+	if err2 != nil {
+		fmt.Println("请求报文出错", err2)
+	}
+	result, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(result))
 
-	// 给服务器发送请求报文
-	dial.Write([]byte(requstHttpHeader))
-	fmt.Fprintln(dial)
-	buf := make([]byte, 1024) //这就是赋值空间
-	fmt.Println("连接服务器1111")
-	// 读取服务器的回复
-	read, err := dial.Read(buf)
-	errFunction("dial.Read err：", err)
-	fmt.Println(string(buf[:read]))
 	fmt.Println("连接服务器222")
 }
 
--- client/clientoperate.go
+++ client/clientoperate.go
@@ -5,23 +5,28 @@ import (
 	"fmt"
 )
 
+var opera client
+
 func operate() {
 	var model int
 	fmt.Println("输入操作的功能：")
 	//	fmt.Scan(&model)
-	model = 1
+	model = 1 //选择插入数据
 	switch model {
 	case 1:
 		var Inname, Incode = input()
-		clientaddget(Inname, Incode)
+		opera.Nameo = Inname
+		opera.Code = Incode
+		opera.Flagnum = 1
+		clientaddget(opera)
 	}
 }
 
 func input() (name, passcode string) {
 	fmt.Println("请输入你的用户名、用户密码：")
 	//_, _ = fmt.Scan(&name, &passcode) // 输入一个后回车再输入下一个
-	name = "sdfgh"
-	passcode = "asdf52"
+	name = "有fv"
+	passcode = "56三"
 	fmt.Printf("用户名：%v\n用户密码：%v\n", name, passcode)
 	return
 }
